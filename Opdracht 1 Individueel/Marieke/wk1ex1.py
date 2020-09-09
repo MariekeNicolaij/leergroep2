f72862d541bf010e302f644118c32f47b2aeeca3
Marieke <mariekenicolay@hotmail.com>
2020-09-09 14:31:58 +0200

    Opdracht vier vieren af. Tot en met 100 yeehaw

--- Opdracht 1 Individueel/Marieke/wk1ex1.py
+++ Opdracht 1 Individueel/Marieke/wk1ex1.py
@@ -1,54 +1,108 @@
-# Het vier-vierenprobleem! Voeg nu een aantal regels toe die lijken op die met nul om de waardes van 0 tot en met 20 te berekenen met precies vier vieren. (Je kan ook een voldoende halen als je er een aantal overslaat waar je niet uitkomt!) Je mag alle rekenkundige bewerkingen van Python gebruiken:
-# + optellen
-# - aftrekken (of getal negatief maken)
-# * vermenigvuldigen
-# / delen
-# ( ) haakjes om de volgorde van de berekening aan te passen
-# ** machtsverheffen
-# Resultaten met een decimale punt: 1.0, 2.0, enz. zijn geen probleem!
-# Je mag ook 44 of 4.4 gebruiken, die ieder als twee vieren tellen,
-# of .4, die als één vier telt.
-# of sqrt, bijvoorbeeld sqrt(4) (of andere wortels)
-# De waarde van sqrt(4), de wortel van 4, is dat getal wat je met zichzelf moet vermenigvuldigen om 4 te krijgen. 2*2 == 4, dus sqrt(4) == 2. Een ander voorbeeld: sqrt(9) == 3, want 3*3 == 9.
-# of factorial, bijvoorbeeld factorial(4) (of andere faculteiten)
-# De waarde van factorial(4), 4 faculteit (ook wel geschreven als 4!) is 4*3*2*1, dus 24. Net zo is factorial(2) gelijk aan 2*1, dus 2, en factorial(5) gelijk aan 5*4*3*2*1, dus 120.
-# sqrt en factorial komen allebei uit de module math van Python
-# terzijde, de regel from math import * zorgt dat deze module beschikbaar is in de code
+import math
 
-# Dit is hoe het resultaat, maar niet je broncode, eruit komt te zien.
+print("Nul = ", 44 - 44)
+print("Een = ", 44 / 44)
+print("Twee = ", 4 / 4 + 4 / 4)
+print("Drie = ", (4 + 4 + 4) / 4)
+print("Vier = ", 4 * (4 - 4) + 4)
+print("Vijf = ", (4 * 4 + 4) / 4)
+print("Zes = ", 4 * .4 + 4.4)
+print("Zeven = ", 44 / 4 - 4)
+print("Acht = ", 4 + 4.4 - .4)
+print("Negen = ", 4 / 4 + 4 + 4)
+print("Tien = ", 44 / 4.4)
+print("Elf = ", 4/.4 + 4/4)
+print("Twaalf = ", (44 + 4) / 4)
+print("Dertien = ", math.factorial(4) - 44/4)
+print("Veertien = ", 4 * (4 - .4) - .4)
+print("Vijftien = ", 44 / 4 + 4)
+print("Zestien = ", .4 * (44 - 4))
+print("Zeventien = ", 4/4 + 4 * 4)
+print("Achttien = ", 44 * .4 + .4)
+print("Negentien = ", math.factorial(4) - 4 - 4/4)
+print("Twintig = ", 4 * (4/4 + 4))
+print(" ")
+print("21 =", (4.4 + 4)/.4)
+print("22 =", 44 * math.sqrt(4) / 4)
+print("23 =", (4 * math.factorial(4) - 4)/ 4)
+print("24 =", 4 * 4 + 4 + 4)
+print("25 =", (4 * math.factorial(4) + 4) / 4)
+print("26 =", 4/.4 + 4*4)
+print("27 =", 4 - 4/4 + math.factorial(4))
+print("28 =", 44 - 4 * 4)
+print("29 =", 4/.4/.4 + 4)
+print("30 =", (4 + 4 + 4) / .4)
+print("31 =", (math.factorial(4) + 4) / 4 + math.factorial(4))
+print("32 =", 4 * 4 + 4 * 4)
+print("33 =", (4 - .4)/.4 + math.factorial(4))
+print("34 =", 44 - 4/.4)
+print("35 =", 44 / 4 + math.factorial(4))
+print("36 =", 44 - 4 - 4)
+print("37 =", (math.sqrt(4) + math.factorial(4))/math.sqrt(4) + math.factorial(4))
+print("38 =", 44 - math.factorial(4)/4)
+print("39 =", (4 * 4 - .4)/.4)
+print("40 =", 44 - math.sqrt(4 * 4))
+print(" ")
+print("41 =", (math.sqrt(4) + math.factorial(4))/.4 - math.factorial(4))
+print("42 =", math.sqrt(4) + 44 - 4)
+print("43 =", 44 - 4/4)
+print("44 =", 44.4 - .4)
+print("45 =", 4/4 + 44)
+print("46 =", 44 - math.sqrt(4) + 4)
+print("47 =", math.factorial(4) + math.factorial(4) - 4/4)
+print("48 =", 4 * (4 + 4 + 4))
+print("49 =", (math.factorial(4) - 4.4) / .4)
+print("50 =", math.factorial(4) / 4 + 44)
+print(" ")
+print("51 =", (math.factorial(4) - 4 + .4) / .4)
+print("52 =", 44 + 4 + 4)
+print("53")
+print("54 =", math.factorial(4) + math.factorial(4) + (math.factorial(4) / 4))
+print("55")
+print("56 =", 44 + math.factorial(4) / math.sqrt(4))
+print("57")
+print("58 =", math.factorial(4) + math.factorial(4) + 4/.4)
+print("59 =", math.factorial(4)/.4 - 4/4)
+print("60 =", 44 + 4 * 4)
+print("61 =", math.factorial(4)/.4 + 4/4)
+print("62 =", (4 + 4) ** math.sqrt(4) - math.sqrt(4))
+print("63 =", (math.factorial(4) + math.sqrt(4)) / .4 - math.sqrt(4))
+print("64 =", 4 * 4 * math.sqrt(4) * math.sqrt(4))
+print("65 =", (4**4 +4) / 4)
+print("66 =", (4 + 4) ** (math.sqrt(4)) + math.sqrt(4))
+print("67 =", (math.factorial(4) + math.sqrt(4)) / .4 + math.sqrt(4))
+print("68 =", (4 + 4) ** math.sqrt(4) + 4)
+print("69 =", (math.factorial(4) + 4 - .4) / .4)
+print("70 =", 44 + math.factorial(4) + math.sqrt(4))
+print("71 =", (4 + math.factorial(4) + .4) / .4)
+print("72 =", (math.factorial(4) / 4) ** math.sqrt(4) * math.sqrt(4))
+print("73")
+print("74 =", math.factorial(4) + math.factorial(4) + math.factorial(4) + math.sqrt(4))
+print("75 =", (math.factorial(4) + math.sqrt(4) + 4) / .4)
+print("76 =", math.factorial(4) * 4 - math.factorial(4) + 4)
+print("77")
+print("78")
+print("79")
+print("80 =", math.factorial(4) * 4 - 4*4)
+print("81")
+print("82 =", math.factorial(4) / .4 + math.factorial(4) - math.sqrt(4))
+print("83")
+print("84 =", math.factorial(4) * 4 - math.factorial(4) / math.sqrt(4))
+print("85")
+print("86 =", math.factorial(4) * 4 - 4/.4)
+print("87")
+print("88 =", math.factorial(4) * 4 - 4 - 4)
+print("89 =", math.factorial(4) + (math.factorial(4) + math.sqrt(4)) / .4)
+print("90 =", math.factorial(4) * 4 - math.factorial(4) / 4)
+print("91 =", 4 * math.factorial(4) - math.sqrt(4) / .4)
+print("92 =", math.factorial(4) * (math.sqrt(4) + math.sqrt(4)) - 4)
+print("93")
+print("94 =", math.factorial(4) * 4 - 4/math.sqrt(4))
+print("95 =", math.factorial(4) * 4 - 4/4)
+print("96 =", math.factorial(4) * 4 + 4 - 4)
+print("97 =", math.factorial(4) * 4 + 4/4)
+print("98 =", math.factorial(4) * 4 + 4/math.sqrt(4))
+print("99")
+print("100 =", math.factorial(4) * 4 + math.sqrt(4) + math.sqrt(4))
 
-# Tip: vergeet sqrt en factorial niet!
 
-# Nul is 0
-# Een is 1
-# Twee is 2
-# Drie is 3
-# Vier is 4
-# Vijf is 5
-# Zes is 6
-# Zeven is 7
-# Acht is 8
-# Negen is 9
-# Tien is 10
-# Elf is 11
-# Twaalf is 12
-# Dertien is 13
-# Veertien is 14
-# Vijftien is 15
-# Zestien is 16
-# Zeventien is 17
-# Achttien is 18
-# Negentien is 19
-# Twintig is 20
-
-# Bonuskarma: als je wilt, mag je verder gaan dan 20.
-# We willen dit niet te veel aanmoedigen (er zijn andere dingen om te doen!)
-
-# Als je je resultaten hebt kan je je bestand wk1ex1.py inleveren.
-# Bij deze workshops werken we met github. Maak, als je dat nog niet hebt, een account aan op deze site.
-# Vervolgens kun je een project aanmaken (bijvoorbeeld Workshops_DT) en daar je bestanden uploaden.
-# We komen in week vier uitgebreid terug op de vraag waarom dit handig is; vertrouw er nu maar even op dat dit je leven veel aangenamer gaat maken.
-# De bestandsnaam maakt uit! Zorg ervoor dat je bestand wk1ex1.py heet.
-# Dit is met het cijfer 1, niet de kleine letter L
-# Geef het bij het inleveren aan als je met iemand hebt samengewerkt
-# Deze opgave moet je alleen maken, zodat je in ieder geval zelf een werkende Python-installatie hebt.
