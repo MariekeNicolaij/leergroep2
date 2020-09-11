aadb9ef330b15bc498bd15e08467c464c2e21693
Marieke <mariekenicolay@hotmail.com>
2020-09-11 14:08:27 +0200

    Finished assignment 2a

--- Opgave 2/opgave2a.py
+++ Opgave 2/opgave2a.py
@@ -13,15 +13,15 @@ def rps():
         results: no results        (printing doesn't count as a result)
     """
     # Gebruiker uitnodigen
-    print('Kom tot ziens kom tot ziens! Steen papier schaar!!!!')
-    time.sleep(1)
+    print('Kom dat zien kom dat zien! Battle of the leergroep 2 en iemand anders!!!!')
+    print()
     # answer = input('Wilt u een potje spelen?')
     time.sleep(2)
 
     letsplay = True
     while letsplay:
         yesno = input ("Wil je mee doen?: [Ja/Nee]? : ")
-        # check if d1a is equal to one of the strings, specified in the list
+        # check if yesno is equal to one of the strings, specified in the list
         if yesno in ['Ja', 'Nee']:
             # if it was equal - break from the while loop
             break
@@ -30,22 +30,85 @@ def rps():
             print ("Oke doei") 
             # quit() werkt niet ?
             letsplay = False
-        elif yesno.lower() == "ja": 
+        elif yesno.lower() == "ja" and 'yes': 
             print ("Have fun!")
             break
+    print()
 
 # small fix because quit() doesn't work 
     if (not letsplay):
         return
-    user = input("Kies je wapen [steen, papier, schaar]: ")
-    comp = random.choice(['steen', 'papier', 'schaar'])
+
+    weaponIsChosen = False
+    weapon1 = 'Tiny Tim'
+    weapon2 = 'Trotse Tom'
+    weapon3 = 'Makkelijke Marieke'
+    weapon4 = 'Grote boze heks'
+    weapon = ''
+
+    print()
+    while not weaponIsChosen:
+        weapon = input ("Choose your weapon: [" + weapon1 + "/" + weapon2 + "/" + weapon3 + "/" + weapon4 + "/" + "]? : ")
+        # check if yesno is equal to one of the strings, specified in the list
+        if weapon in [weapon1, weapon2, weapon3, weapon4]:
+            # if it was equal - break from the while loop
+            # process the input
+            if weapon == weapon1: 
+                print("Ik vind provotype een beter woord")
+                weaponIsChosen = True
+            elif weapon == weapon2: 
+                print("9 tegen rascisme")
+                weaponIsChosen = True
+            elif weapon == weapon3: 
+                print("Roekoe HOOO")
+                weaponIsChosen = True
+            elif weapon == weapon4: 
+                print("IPOOOOOOOOO")
+                weaponIsChosen = True
+
+    comp = random.choice([weapon1, weapon2, weapon3, weapon4])
     print()
 
-    print('De gebruiker (jij) koos', user)
+    print('De gebruiker (jij) koos', weapon)
     print('De computer (ik)   koos', comp)
     print()
 
-    if user == 'steen':
-        print('Haha! Eigenlijk koos ik papier! IK HEB GEWONNEN!')
+    if weapon == weapon1:
+        if comp == weapon1:
+            print("Gelijkspel! Probeer nog een keer")
+        if comp == weapon2:
+            print("Speler heeft gewonnen!")
+        if comp == weapon3:
+            print("Computer heeft gewonnen!")
+        if comp == weapon4:
+            print("Speler heeft gewonnen!")
 
-    print("Hopelijk heb je de volgende keer meer geluk...")
+    if weapon == weapon2:
+        if comp == weapon1:
+            print("Speler heeft gewonnen!")
+        if comp == weapon2:
+            print("Speler heeft gewonnen!")
+        if comp == weapon3:
+            print("Speler heeft gewonnen!")
+        if comp == weapon4:
+            print("Speler heeft gewonnen!")
+
+    if weapon == weapon3:
+        if comp == weapon1:
+            print("Speler heeft gewonnen!")
+        if comp == weapon2:
+            print("Computer heeft gewonnen!")
+        if comp == weapon3:
+            print("Gelijkspel! Probeer nog een keer")
+        if comp == weapon4:
+            print("Speler heeft gewonnen!")
+
+    if weapon == weapon4:
+        if comp == weapon1:
+            print("Computer heeft gewonnen!")
+        if comp == weapon2:
+            print("Computer heeft gewonnen!")
+        if comp == weapon3:
+            print("Computer heeft gewonnen!")
+        if comp == weapon4:
+            print("Computer heeft gewonnen!")
