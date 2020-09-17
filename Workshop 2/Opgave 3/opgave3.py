1913e35d87d2caaf49be70093c152dc39e235f83
Marieke <mariekenicolay@hotmail.com>
2020-09-17 14:49:31 +0200

    Added opgave3 and played with the given code

---
+++ Workshop 2/Opgave 3/opgave3.py
@@ -0,0 +1,22 @@
+
+from math import *
+
+def dbl(x):
+    """Returns twice the argument
+
+    Spam is great, and dbl("spam") is better!
+
+    :param x: The value to double
+    :type x: int, float or string
+    :rtype: int, float or string
+    """
+    return 2 * x
+
+def tpl(x):
+    """Returns thrice the argument
+
+    :param x: The value to triple
+    :type x: int, float or string
+    :rtype: int, float or string
+    """
+    return 3 * x
