96e1bd3eb9e023c3cf3b17a61c98124afc31d8e3
tom13m <70517942+tom13m@users.noreply.github.com>
2020-09-18 15:38:43 +0200

    Alleen de laatste opgave moet nog

--- Workshop 2/Opgave 4/wk2ex3.py
+++ Workshop 2/Opgave 4/wk2ex3.py
@@ -25,13 +25,69 @@ def dot(L, k):
     else:
         return 0
 
-# def ind(e, L):
-#     """Function for calculating the inproduct of 
-#         two given lists
+def ind(e, L):
+    """Function for seeking the first common index
+        in a string or list
+    """
+    length = len(L) - 1
+
+    if e not in L :
+        return length
+    elif type(L) == str or type(L) == list :
+        if (e == L[length]) :
+            print(length)
+            return length
+        else :
+            ind(e, L[:-1])
+
+def letter_score(let):
+    """Function for indicating the score of a given
+        Scrabble letter
+    """
+    if type(let) == str and len(let) == 1:
+        let = let.lower()
+
+        array = [ ['adeinorst', 1], ['ghl' , 2], ['bcmp', 3], ['jkuvw', 4], ['f', 5], ['z', 6], ['xy', 8], ['q', 10] ]
+       
+        for i in array:
+            if let in i[0]:
+                return i[1]
+    else:
+        return 0
+
+def scrabble_score(s):
+    """Function for indicating the score of a given
+        Scrabble word
+    """
+    if type(s) == str and len(s) > 0:  
+        s = s.lower()
+        array = [ ['adeinorst', 1], ['ghl' , 2], ['bcmp', 3], ['jkuvw', 4], ['f', 5], ['z', 6], ['xy', 8], ['q', 10] ]
+        score = 0
+
+        for i in array:
+            if s[0] in i[0]:
+                score += i[1]
+        
+        return score + scrabble_score(s[1:])
+    else:
+        return 0   
+
+# def transcribe(s):
+#     """Function for indicating the score of a given
+#         Scrabble word
 #     """
-#     if e not in L:
-#         return len(L)
-#     elif type(L) == string:
-#         print('string')
-#     elif type(L) == list:
-#         print('list')
+#     if type(s) == str and len(s) > 0:
+#         s = s.lower()
+#         s = s.strip()
+
+#         array = [['a', 'u'], ['c', 'g'], ['g', 'c'], ['t', 'a']]
+
+#         transcription = ''
+
+#         for i in array:
+#             if s == i[0]:
+#                 transcription = transcription + i[1]
+
+#         return transcription + transcribe(s[1:])
+#     else:
+#         return ''
