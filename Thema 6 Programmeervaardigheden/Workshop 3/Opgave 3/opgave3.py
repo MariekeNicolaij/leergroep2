3af34fa1cd6c129410bb6bdba97f08019d8e6117
Marieke <mariekenicolay@hotmail.com>
2020-10-08 20:20:44 +0200

    lingooooooooo

--- Thema 6 Programmeervaardigheden/Workshop 3/Opgave 3/opgave3.py
+++ Thema 6 Programmeervaardigheden/Workshop 3/Opgave 3/opgave3.py
@@ -4,7 +4,7 @@
 # De functie encipher moet een nieuwe string teruggeven waarin de letters
 # in s met n letters voorwaarts zijn "geroteerd" in het alfabet, 
 # en terug naar het begin van het alfabet als dat nodig is.
-def encipher(string, number):
+def encipher(string = "ondo", number = 1):
     # if number < 0:
     #     number = 0
     if number > 25:
@@ -28,18 +28,33 @@ def encipher(string, number):
 
         temp += abc[index]
         i = i + 1
-
     return temp
 
-def decipher(s):
+def decipher(s = "qpfq"):
     return encipher(s, -1)
 
 def blsort(L = [0,0,1,1,0,1,0,1,1,1,1,1]):
     return [0 for x in L if x == 0] + [1 for x in L if x == 1]
 
-def count(e, L):
-    lc = [1 for x in L if x == e]
-    return len(lc)
+import random
 
-def gensort(L):
-    return L
+def gensort(L = [random.randrange(0, 100),random.randrange(0, 100),random.randrange(0, 100),random.randrange(0, 100),random.randrange(0, 100),random.randrange(0, 100),random.randrange(0, 100),random.randrange(0, 100),random.randrange(0, 100),random.randrange(0, 100)], temp = []):
+   
+    if len(L) == 0:
+        return temp
+    else:
+        temp.append(min(L)) # add smallest item to temp list
+        L.remove(min(L)) # remove smallest item from original list
+        return gensort(L, temp) # opnieuw!
+
+def lingo(string = "poephhhhh", tring = "hanze"):
+    if not string or not tring:
+        return 0
+
+    similarLetters = 0
+    for s in string:
+        if s in tring:
+            tring = tring.replace(s, '', 1) # Remove first occurence of s
+            similarLetters += 1
+
+    return similarLetters
