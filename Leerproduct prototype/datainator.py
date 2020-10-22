dcde95accc0c6ac623f7bc2ade68e3953fb8bb04
Marieke <mariekenicolay@hotmail.com>
2020-10-22 21:24:30 +0200

    Grafieken laten nu alleen data van dezelfde week zien

--- Leerproduct prototype/datainator.py
+++ Leerproduct prototype/datainator.py
@@ -2,51 +2,44 @@ import os.path
 import json
 import math
 
+# gemeente_veiligheidsregio.json komt hier weg: https://github.com/minvws/nl-covid19-data-dashboard/blob/master/src/data/gemeente_veiligheidsregio.json
+# overige data komt hier weg: https://github.com/boisei0/corona-dashboard-data/tree/master/data/2020-10-07_1
+
 from datetime import datetime
 import numpy as np
 from matplotlib import pyplot as plt
 
 # stap 1: open en lezen van gemeente-veiligheidsregio's
-# dit bestand komt hier weg: https://github.com/minvws/nl-covid19-data-dashboard/blob/master/src/data/gemeente_veiligheidsregio.json
 with open('data/gemeente_veiligheidsregio.json', 'r') as f:
     regio_data = json.load(f)
 
-# print (type(regio_data)) # list
-#print (regio_data[1])
-#print (regio_data[2])4
-
-
-# Nu kun je niet een niet bestaande stad invullen
+# stap 2: zoek de VR (veiligheidsregio) van 'input'' op
 regios = ''
-while not regios:
-    # stap 2: zoek de VR (veiligheidsregio) van Groningen op
-    # stad = "Groningen"
-    stad = input("Geef uw stad op: ")
-    stad = stad.capitalize()  # Eerste letter = caps
+while not regios: # Nu kun je niet een niet bestaande stad invullen
+    gemeente = input("Geef uw gemeente op: ")
+    gemeente = gemeente.capitalize()  # Eerste letter = caps
     print('')  # Beetje whitespace tussen de teksten door
-    regios = [x for x in regio_data if x['name'] == stad]
+    regios = [x for x in regio_data if x['name'] == gemeente]
 regio = regios[0]['safetyRegion']
-print("De veiligheidsregio nummer van " + stad + " is: " + regio)
+print("De veiligheidsregio nummer van " + gemeente + " is: " + regio)
 print('')  # Beetje whitespace tussen de teksten door
 # VR van Groningen = VR01
 
 # stap 3: welke steden zitten er allemaal in dezelfde VR?
-steden = [x for x in regio_data if x['safetyRegion'] == regio]
+gemeentes = [x for x in regio_data if x['safetyRegion'] == regio]
 print("Gemeentes in deze regio: ")
-for number, ss in enumerate(steden):
-    print(number+1, '\t' + ss['name'])  # Wil niet bij '0' beginnen vandaar +1
+for number, ss in enumerate(gemeentes):
+    print(number+1, '\t' + ss['name'])  # Wil niet dat lijst bij '0' beginnen vandaar +1
 print('')  # Beetje whitespace tussen de teksten door
 
-# Nu kun je geen verkeerde datum invullen
-file_name = 'data/{}.json'.format(regio)
-
 # Data ophalen en storen in arrays
+file_name = 'data/{}.json'.format(regio)
 with open(file_name, 'r') as f:
     data = json.load(f)
 
 region_results = data['results_per_region']['values']
 
-dataArray = {
+data_array = {
     'date_of_report_unix': [],
     'vrcode': [],
     'total_reported_increase_per_region': [],
@@ -65,96 +58,133 @@ dates = []
 for x in region_results:
     for key in x:
         if (key == 'date_of_report_unix'):
-            dataArray[key].append(x[key])
+            data_array[key].append(x[key])
 
             unixDate = datetime.utcfromtimestamp(x[key]).strftime('%Y-%m-%d')
             dates.append(unixDate)
         else:
-            dataArray[key].append(x[key])
+            data_array[key].append(x[key])
 
+data_array_length = len(data_array['date_of_report_unix']) # is toch allemaal hetzelfde
+
+# stap 4: vul een datum in
 datum = ''
-while datum not in dates :
+while datum not in dates: # Je kunt alleen een bestaande datum vinden
     datum = input('Geef een geldige datum in het yyyy-mm-dd format op:')
 
-# Write data to console view
-print('--------------------------------------------------')
-print("Gemeente: " + stad + "\t" + "Bijgewerkt: " + dates[-1])
-print('')
+# ---------- Print totals en averages ----------
 
-total = 0
+#region print_totals_and_averages
+    print('--------------------------------------------------')
+    print("Gemeente: " + gemeente + "\t" + "Datum ingevuld: " + datum + "\t" + "Laatst bijgewerkt: " + dates[-1])
+    print('')
 
-# total_reported_increase_per_region
-for x in dataArray['total_reported_increase_per_region']:
-    total = total + x
-average = total / len(dataArray['total_reported_increase_per_region'])
+    total = 0
 
-print("Total reported: ", total)
-print("Average per day: ", round(average))
-print('')
+    # total_reported_increase_per_region
+    for x in data_array['total_reported_increase_per_region']:
+        total = total + x
+    average = total / len(data_array['total_reported_increase_per_region'])
+    total = 0
 
-# infected_total_counts_per_region
-for x in dataArray['infected_total_counts_per_region']:
-    total = total + x
-average = total / len(dataArray['infected_total_counts_per_region'])
+    print("Total reported: ", total)
+    print("Average per day: ", round(average))
+    print('')
 
-print("Total infected: ", math.floor(total))
-print("Average per day: ", average)
-print('')
+    # infected_total_counts_per_region
+    for x in data_array['infected_total_counts_per_region']:
+        total = total + x
+    average = total / len(data_array['infected_total_counts_per_region'])
+    total = 0
 
-# hospital_total_counts_per_region
-for x in dataArray['hospital_total_counts_per_region']:
-    total = total + x
-average = total / len(dataArray['hospital_total_counts_per_region'])
+    print("Total infected: ", math.floor(total))
+    print("Average per day: ", average)
+    print('')
 
-print("Total in hospital: ", math.floor(total))
-print("Average per day: ", average)
-print('')
+    # hospital_total_counts_per_region
+    for x in data_array['hospital_total_counts_per_region']:
+        total = total + x
+    average = total / len(data_array['hospital_total_counts_per_region'])
+    total = 0
 
-# infected_increase_per_region
-for x in dataArray['infected_increase_per_region']:
-    total = total + x
-average = total / len(dataArray['infected_increase_per_region'])
+    print("Total in hospital: ", math.floor(total))
+    print("Average per day: ", average)
+    print('')
 
-print("Total infected increase: ", math.floor(total))
-print("Average per day: ", average)
-print('')
+    # infected_increase_per_region
+    for x in data_array['infected_increase_per_region']:
+        total = total + x
+    average = total / len(data_array['infected_increase_per_region'])
+    total = 0
 
-# hospital_increase_per_region
-for x in dataArray['hospital_increase_per_region']:
-    total = total + x
-average = total / len(dataArray['hospital_increase_per_region'])
+    print("Total infected increase: ", math.floor(total))
+    print("Average per day: ", average)
+    print('')
 
-print("Total hospital increase: ", math.floor(total))
-print("Average per day: ", average)
-print('')
+    # hospital_increase_per_region
+    for x in data_array['hospital_increase_per_region']:
+        total = total + x
+    average = total / len(data_array['hospital_increase_per_region'])
+    total = 0
 
-# hospital_moving_avg_per_region
-for x in dataArray['hospital_moving_avg_per_region']:
-    total = total + x
-average = total / len(dataArray['hospital_moving_avg_per_region'])
+    print("Total hospital increase: ", math.floor(total))
+    print("Average per day: ", average)
+    print('')
 
-print("Total hospital moving average: ", math.floor(total))
-print("Average per day: ", average)
-print('')
+    # hospital_moving_avg_per_region
+    for x in data_array['hospital_moving_avg_per_region']:
+        total = total + x
+    average = total / len(data_array['hospital_moving_avg_per_region'])
+    total = 0
 
-# Bereken totale rna per ml van gegeven data en regio
-# for d in data['results_per_sewer_installation_per_region']['values'][0]['values']:
-#     TOTAL_rna = TOTAL_rna + d['rna_per_ml']
+    print("Total hospital moving average: ", math.floor(total))
+    print("Average per day: ", average)
+    print('')
 
-# print("Total ribonucleïnezuur (rna) per ml", TOTAL_rna)
-print('--------------------------------------------------')
-print('')
-print('')
+    # Bereken totale rna per ml van gegeven data en regio
+    # for d in data['results_per_sewer_installation_per_region']['values'][0]['values']:
+    #     TOTAL_rna = TOTAL_rna + d['rna_per_ml']
+    # print("Total ribonucleïnezuur (rna) per ml", TOTAL_rna)
+    print('--------------------------------------------------')
+    print('')
+    print('')
+    #endregion
+
+# ---------- Grafieken ----------
+
+# we willen data laten van + 3 dagen en - 3 dagen laten zien
+# plus wat checks zodat er altijd 7 dagen worden weergeven
+dagen_offset_index = 3
+datum_middle_index = dates.index(datum)
+# als de middelste index lager is dan de offset dan wordt het de offset
+# Dus als de gebruiker dag dag 2 of dag 3 van de hele data reeks somehow invuld dan willen we dat het opzchuift zodat we nog mooi 7 dagen kunnen zien
+datum_middle_index = dagen_offset_index if datum_middle_index < dagen_offset_index else datum_middle_index
+
+# hier er dus verder vor zorgen dat de 7 dagen goed geshowed worden
+datum_max_index = (datum_middle_index + dagen_offset_index) if (datum_middle_index + dagen_offset_index) < data_array_length else (data_array_length - 1) # index
+datum_min_index = (datum_max_index - 6) if (datum_max_index - 6) > 0 else 0 # 7 days in a week en 1 index ding
 
 
-# eem wat proberen
-
-x = np.arange(0,DATA_length) 
-# y = 2 * x + 5 
-y = [i for i in DATA_total_reported_increase_per_region]
-plt.title(stad + " " + datum) 
-plt.xlabel("Data object #") 
-plt.ylabel("DATA_total_reported_increase_per_region") 
+x = dates[datum_min_index:datum_max_index + 1] # wil niet dat ie de laatste exclude
+y = data_array['total_reported_increase_per_region'][datum_min_index:datum_max_index + 1] # wil niet dat ie de laatste exclude
+plt.title(gemeente + ": total_reported_increase_per_region") 
+plt.xlabel("Datum") 
+plt.ylabel("Waarde") 
 plt.plot(x,y,"ob") 
-
 plt.show()
+
+x = dates[datum_min_index:datum_max_index + 1] # wil niet dat ie de laatste exclude
+y = data_array['hospital_total_counts_per_region'][datum_min_index:datum_max_index + 1] # wil niet dat ie de laatste exclude
+plt.title(gemeente + ": hospital_total_counts_per_region") 
+plt.xlabel("Datum") 
+plt.ylabel("Waarde") 
+plt.plot(x,y,"ob") 
+plt.show()
+
+# Tim kan jij er voor zorgen: 
+# * dat er lijntjes tussen de punten komen
+# * dat de gekozen 'datum' een kleurtje krijgt (is niet heel belangrijk)
+# * dat het grafiek venster iets groter is zodat die datums niet zo over elkaar heen lopen 
+# * zou heel vet zijn als je voor elkaar weet te krijgen hoe we alle grafieken in 1 scherm 
+# kunnen houden ipv dat het steeds een nieuw scherm opent als je op kruisje klikt. Dit is mij
+# ook niet gelukt hoor. Iets met 'plt.subplot'
